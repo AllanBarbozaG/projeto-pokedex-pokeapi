31c5ccc3476b42e14ad56c584e5663bd1ea6e943
allanbarbozag <allpatrick.b@gmail.com>
2022-08-12 23:02:03 -0300

    feat: adiciona eventListener no input de nome que o usuario preenche, para capturar o nome e enviar à requisição da API. Adiciona variáveis para selecionar os atributos: nome, id, moves e gif do pokemon.

--- app.js
+++ app.js
@@ -1,29 +1,50 @@
-const recebePoke = 'ditto'
-const requisicao = () => fetch(`https://pokeapi.co/api/v2/pokemon/${recebePoke}`)
+const requisicao = () => fetch(`https://pokeapi.co/api/v2/pokemon/${pesquisar.value}`)
 .then(response => response.json())
 .then(data => {
-  console.log(data)
-  return data
+  console.log(data);
+  return data;
 }) 
-.catch(error => console.log(error)) 
+.catch(error => console.log(error));
+
+const pesquisar = document.querySelector('#input-usuario-nome-poke');
+
+pesquisar.addEventListener('input', async () => {
+  const req = await requisicao()
+  console.log(pesquisar.value)
+
+  const img = document.querySelector('#img')
+  img.innerHTML = 
+  `
+  <img src='${req.sprites.versions['generation-v']['black-white'].animated.front_default}'>
+  `
+
+})
+
+
+
+
 
 
 async function retornaInformacoes() {
-  const req = await requisicao()
+  const req = await requisicao();
 
-  const name = document.querySelector('.name')
-  name.innerText = `${req.name}`
+  const name = document.querySelector('.name');
+  name.innerText = `${req.name}`;
 
-  const id = document.querySelector('.id')
-  id.innerText = `${req.id}`
+  const id = document.querySelector('.id');
+  id.innerText = `${req.id}`;
 
-  const moves = document.querySelector('.moves')
-  moves.innerText = `${req.moves}`
-  
+  const moves = document.querySelector('.moves');
+  moves.innerText = `${req.moves[0].move.name}`;
 
+  console.log(req.sprites.versions['generation-v']['black-white'].animated.front_default);
+  const img = document.querySelector('#img');
+  img.innerHTML =
+  `
+   <img src='${req.sprites.versions['generation-v']['black-white'].animated.front_default}'>
+  `;  
 } 
 
-retornaInformacoes()
 
 
 
