39fbd9c7866f304cfa07ea304755cce5ea54fdd4
allanbarbozag <allpatrick.b@gmail.com>
2022-08-13 18:08:32 -0300

    refactor: modifica o código do arquivo app.js para deixar apenas o essencial (requisição e eventListener) e implementa os códigos refatorados que saíram do app.js nos separados nos arquivos PokemonController.js e PokemonView.js.

--- app.js
+++ app.js
@@ -3,59 +3,11 @@ const pesquisar = document.querySelector('#input-usuario-nome-poke');
 const requisicao = () => fetch(`https://pokeapi.co/api/v2/pokemon/${pesquisar.value}`)
 .then(response => response.json())
 .then(data => {
-  console.log(data);
   return data;
 }) 
 .catch(error => console.log(error));
 
-async function retornaInformacoes() {
-  const req = await requisicao();
-
-  const name = document.querySelector('.name');
-  name.innerText = `Nome: ${req.name}`;
-
-  const id = document.querySelector('.id');
-  id.innerText = `ID: ${req.id}`;
-
-  const moves = document.querySelector('.moves');
-  moves.innerText = `${req.moves[0].move.name}`;
-
-  
-} 
-
-pesquisar.addEventListener('input', async () => {
-  
-  const req = await requisicao()
-  console.log(pesquisar.value)
-
-  retornaInformacoes()
-
-  const img = document.querySelector('#img-div')
-
-  try {
-    if(req.sprites.versions['generation-v']['black-white'].animated.front_default) {
-      img.innerHTML = 
-      `
-        <img id="poke-img" src='${req.sprites.versions['generation-v']['black-white'].animated.front_default}'>
-      `;
-      console.log('ok')
-    } else if (req.sprites.front_default) {
-      img.innerHTML = 
-      `
-        <img id="poke-img" src='${req.sprites.front_default}'>
-      `;
-    } else {
-      throw new Error('Pokemon não possui imagem padrão a ser exibida.')
-    }
-
-  } catch(error) {
-    console.log(error)
-  }
-
-  
-  console.log(req.sprites.front_default)
-
-})
+pesquisar.addEventListener('input', () => retornaInformacoes());
 
 
 
